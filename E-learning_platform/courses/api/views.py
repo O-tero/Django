2e6a3ec0667fab74701011dae7e6003001e41045
Otero <oteronickbrian@gmail.com>
2024-02-28 12:37:06 +0300

    adding viewsets and routers

--- E-learning_platform/courses/api/views.py
+++ E-learning_platform/courses/api/views.py
@@ -3,9 +3,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
-
-
+from courses.api.serializers import SubjectSerializer, CourseSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
@@ -16,7 +17,13 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, id=pk)
         course.students.add(request.user)
         return Response({"enrolled": True})
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
