a03cd4fae612b73f88c6bb56459d3249f7873448
Otero <oteronickbrian@gmail.com>
2024-03-09 15:10:47 +0300

    blackboard CMS

---
+++ Blackboard/blackboard/blackboard/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-+ljbb79@^*g@ieh(1bov0imy3=)wad5jb@y)-g8*t-1l!ins=i"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
