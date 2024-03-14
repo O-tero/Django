09cf530ca66cada3c66eb8712334a3ea5e106ae8
Otero <oteronickbrian@gmail.com>
2024-03-15 01:13:47 +0300

    debugging recursion loop

--- E-learning_platform/accounts/urls.py
+++ E-learning_platform/accounts/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
-from django.conf import settings
-from . import views
-from django.conf.urls.static import static
+# from django.urls import path, include
+# from django.conf import settings
+# from . import views
+# from django.conf.urls.static import static
 
-urlpatterns = [
-    path("", views.index, name="index"),
-]
+# urlpatterns = [
+#     path("", views.index, name="index"),
+# ]
